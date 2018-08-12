53c9989babd4321424cc707fd40b2fa1d9ad6455
ByungSoo Ko <kobiso62@gmail.com>
2018-08-12 17:43:17 +0900

    Remove unused method

--- ResNeXt.py
+++ ResNeXt.py
@@ -158,23 +158,7 @@ def main(args):
                     layers_split.append(splits)
 
                 return Concatenation(layers_split)
-
-        def squeeze_excitation_layer(self, input_x, out_dim, ratio, layer_name):
-            with tf.name_scope(layer_name) :
-
-
-                squeeze = Global_Average_Pooling(input_x)
-
-                excitation = Fully_connected(squeeze, units=out_dim / ratio, layer_name=layer_name+'_fully_connected1')
-                excitation = Relu(excitation)
-                excitation = Fully_connected(excitation, units=out_dim, layer_name=layer_name+'_fully_connected2')
-                excitation = Sigmoid(excitation)
-
-                excitation = tf.reshape(excitation, [-1,1,1,out_dim])
-                scale = input_x * excitation
-
-                return scale
-
+                
         def residual_layer(self, input_x, out_dim, layer_num, res_block=blocks):
             # split + transform(bottleneck) + transition + merge
             # input_dim = input_x.get_shape().as_list()[-1]
